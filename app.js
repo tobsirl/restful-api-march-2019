4ce9491ff0ac053b2db8f4b007cf53812a3bad2d
Paul Tobin <tobster111@gmail.com>
2019-03-22 12:17:07 +0000

    Added cors and middleware section to app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 import 'dotenv/config';
 
 const app = express();
+
+// Middleware
+app.use(cors());
 
 app.use('/', (req, res) => {
   res.json({ message: 'route working', name: 'John Doe', age: 25 });
