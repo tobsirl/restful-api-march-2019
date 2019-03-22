30ccf4157943bcbf54afd1e14533ebe93021dac6
Paul Tobin <tobster111@gmail.com>
2019-03-22 11:56:19 +0000

    Added .env file and installed dotenv to import

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 
+import 'dotenv/config';
+
 const app = express();
 
 app.use('/', (req, res) => {
   res.json({ message: 'route working', name: 'John Doe', age: 25 });
 });
 
-const PORT = 3000;
+const { PORT } = process.env;
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT} :rocket: `);
